a86867ad22511e7ca6ea8ed394866bcd2e420714
vincent-picaud <picaud.vincent@gmail.com>
2021-12-11 23:02:51 +0100

    Improve export_png()

--- src/export.jl
+++ src/export.jl
@@ -3,7 +3,13 @@ export export_png
 function export_png(gp::GnuPlotScript,filename::AbstractString)
     _append_to_script(gp,"set terminal push")
     _append_to_script(gp,"set terminal png")
-    filename = first(split(filename))*".png"
+
+    filename = splitext(filename)
+    if length(filename)>1 && filename[2]!=".png"
+        @warn "File extension $(filename[2]) replaced by \".png\""
+    end
+    filename = first(filename)*".png"
+
     _append_to_script(gp,"set output '$filename'")
     _append_to_script(gp,"replot")
     _append_to_script(gp,"set terminal pop")
