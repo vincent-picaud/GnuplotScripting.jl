ea7b27e7a53d8a0803fac42a39240c7cf0c7bba0
Documenter.jl <documenter@juliadocs.github.io>
2022-01-18 12:24:13 +0000

    build based on 608fad0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GnuPlotScripting","category":"page"},{"location":"#GnuPlotScripting","page":"Home","title":"GnuPlotScripting","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GnuPlotScripting.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GnuPlotScripting]","category":"page"}]
+}
